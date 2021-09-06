d765d74d13a8f9c4d9ce8a6f190f25b2cb87b59f
51135499 <a172839f@gmail.com>
2021-09-06 18:36:52 +0800

    更新 BitRacerPro2.ts, _locales/zh/BitRacerPro2-strings.json

--- BitRacerPro2.ts
+++ BitRacerPro2.ts
@@ -157,19 +157,36 @@ namespace BitRacerPro2 {
     * 讀取線位置
     */
     //% color=#2080ff
-    //% weight=28
+    //% weight=28 
     //% block="read Line position"
     export function readLine(): number {
         pins.i2cWriteNumber(N76_ADDR, 0x08, NumberFormat.UInt8LE, false)
         return pins.i2cReadNumber(N76_ADDR, NumberFormat.Int16LE, false)
     }
 
-    
+
+    /**
+    * 自走車控制開關
+    * @param status 開關
+    */
+    //% weight=80 group="控制(Control)"
+    //% block="carControl |%status" advanced=true
+    export function carControl(status: Switch): void {
+        pins.i2cWriteNumber(N76_ADDR, 0x56 + status, NumberFormat.UInt8LE, false)
+    }
+    /**
+    * 紅外線循跡開關
+    * @param status 開關
+    */
+    //% weight=79 group="控制(Control)"
+    //% block="IR CorrectionMode |%status" advanced=true
+    export function correctionMode(status: Switch): void {
+        pins.i2cWriteNumber(N76_ADDR, 0x58 + status, NumberFormat.UInt8LE, false)
+    }
     /**
     * 陀螺儀零點校正(執行後須等待1秒)
     */
-    //% color=#3dbf53
-    //% weight=20
+    //% weight=78 group="控制(Control)"
     //% block="Set Gyro ZeroPoint" advanced=true
     export function SetZeropoint(): void {
         basic.pause(50)
@@ -179,26 +196,103 @@ namespace BitRacerPro2 {
     /**
     * 清除所有控制變數
     */
-    //% color=#40994f
-    //% weight=20
+    //% weight=77 group="控制(Control)"
     //% block="AllControlClear" advanced=true
     export function AllControlClear(): void {
         pins.i2cWriteNumber(N76_ADDR, 0x21, NumberFormat.UInt8LE, false)
     }
     /**
-    * 清除觀測器數值
+    * 位置控制
+    * @param status 控制狀態
+    */
+    //% weight=75 group="控制(Control)"
+    //% block="positionControl |%status" advanced=true
+    export function positionFlag(status: control): void {
+        pins.i2cWriteNumber(N76_ADDR, 0x60 + status, NumberFormat.UInt8LE, false)
+    }
+    /**
+    * 角度控制
+    * @param status 控制狀態
+    */
+    //% weight=74 group="控制(Control)"
+    //% block="thetaControl |%status" advanced=true
+    export function thetaFlag(status: control): void {
+        pins.i2cWriteNumber(N76_ADDR, 0x66 + status, NumberFormat.UInt8LE, false)
+    }
+    /**
+    * 轉彎方向
+    * @param status 方向
+    */
+    //% weight=73 group="控制(Control)"
+    //% block="turnDirection |%status" advanced=true
+    export function turnDirection(status: direction): void {
+        pins.i2cWriteNumber(N76_ADDR, 0x64 + status, NumberFormat.UInt8LE, false)
+    }
+    /**
+    * 讀取距離命令(mm)
     */
     //% color=#40994f
-    //% weight=20
-    //% block="Observer Clear" advanced=true
-    export function ObserverClear(): void {
-        pins.i2cWriteNumber(N76_ADDR, 0x22, NumberFormat.UInt8LE, false)
+    //% weight=50 group="控制(Control)"
+    //% block="readPosCommand" advanced=true
+    export function readPosCommand(): number {
+        pins.i2cWriteNumber(N76_ADDR, 0x6D, NumberFormat.UInt8LE, false)
+        return pins.i2cReadNumber(N76_ADDR, NumberFormat.Float32LE, false)
+    }
+    /**
+    * 讀取速度命令(m/s)
+    */
+    //% color=#40994f
+    //% weight=49 group="控制(Control)"
+    //% block="readVelCommand" advanced=true
+    export function readVelCommand(): number {
+        pins.i2cWriteNumber(N76_ADDR, 0x6C, NumberFormat.UInt8LE, false)
+        return pins.i2cReadNumber(N76_ADDR, NumberFormat.Float32LE, false)
+    }
+    /**
+    * 讀取估測距離(mm)
+    */
+    //% color=#3dbf53
+    //% weight=48 group="控制(Control)"
+    //% block="Read Observer Distance" advanced=true
+    export function ReadObserverDistance(): number {
+        pins.i2cWriteNumber(N76_ADDR, 0x26, NumberFormat.UInt8LE, false)
+        return pins.i2cReadNumber(N76_ADDR, NumberFormat.Float32LE, false)
+    }
+    /**
+    * 讀取估測速度(m/s)
+    */
+    //% color=#3dbf53
+    //% weight=47 group="控制(Control)"
+    //% block="Read Observer Velocity" advanced=true
+    export function ReadObserverVelocity(): number {
+        pins.i2cWriteNumber(N76_ADDR, 0x27, NumberFormat.UInt8LE, false)
+        return pins.i2cReadNumber(N76_ADDR, NumberFormat.Float32LE, false)
+    }
+    /**
+    * 讀取角度命令
+    */
+    //% color=#40994f
+    //% weight=46 group="控制(Control)"
+    //% block="readThetaCommand" advanced=true
+    export function readThetaCommand(): number {
+        pins.i2cWriteNumber(N76_ADDR, 0x6B, NumberFormat.UInt8LE, false)
+        return pins.i2cReadNumber(N76_ADDR, NumberFormat.Float32LE, false)
+    }
+    /**
+    * 讀取角速度命令
+    */
+    //% color=#40994f
+    //% weight=45 group="控制(Control)"
+    //% block="readOmegaCommand" advanced=true
+    export function readOmegaCommand(): number {
+        pins.i2cWriteNumber(N76_ADDR, 0x6A, NumberFormat.UInt8LE, false)
+        return pins.i2cReadNumber(N76_ADDR, NumberFormat.Float32LE, false)
     }
     /**
     * 讀取Z軸角度數值
     */
     //% color=#3dbf53
-    //% weight=20
+    //% weight=44 group="控制(Control)"
     //% block="read Angle Z" advanced=true
     export function ReadAngleZ(): number {
         pins.i2cWriteNumber(N76_ADDR, 0x23, NumberFormat.UInt8LE, false)
@@ -208,7 +302,7 @@ namespace BitRacerPro2 {
     * 讀取Z軸角速度數值
     */
     //% color=#3dbf53
-    //% weight=20
+    //% weight=43 group="控制(Control)"
     //% block="read Gyro Z" advanced=true
     export function ReadGyroZ(): number {
         pins.i2cWriteNumber(N76_ADDR, 0x24, NumberFormat.UInt8LE, false)
@@ -218,40 +312,21 @@ namespace BitRacerPro2 {
     * 讀取Y軸加速度數值
     */
     //% color=#3dbf53
-    //% weight=20
+    //% weight=42 group="控制(Control)"
     //% block="read Accel Y" advanced=true
     export function ReadAccelY(): number {
         pins.i2cWriteNumber(N76_ADDR, 0x25, NumberFormat.UInt8LE, false)
         return pins.i2cReadNumber(N76_ADDR, NumberFormat.Float32LE, false)
     }
 
-    /**
-    * 讀取估測距離(mm)
-    */
-    //% color=#40994f
-    //% weight=20
-    //% block="Read Observer Distance" advanced=true
-    export function ReadObserverDistance(): number {
-        pins.i2cWriteNumber(N76_ADDR, 0x26, NumberFormat.UInt8LE, false)
-        return pins.i2cReadNumber(N76_ADDR, NumberFormat.Float32LE, false)
-    }
-    /**
-    * 讀取估測速度(mm/ms)
-    */
-    //% color=#40994f
-    //% weight=20
-    //% block="Read Observer Velocity" advanced=true
-    export function ReadObserverVelocity(): number {
-        pins.i2cWriteNumber(N76_ADDR, 0x27, NumberFormat.UInt8LE, false)
-        return pins.i2cReadNumber(N76_ADDR, NumberFormat.Float32LE, false)
-    }
+    
     /**
     * 設定紅外線控制PID參數
     * @param Kp 比例增益, eg: 0
     * @param Ki 積分增益, eg: 0
     * @param Kd 微分增益, eg: 0
     */
-    //% weight=19 group="PID"
+    //% weight=20 group="PID"
     //% block="PID IR Kp|%Kp Ki|%Ki Kd|%Kd" advanced=true
     export function setIR(Kp: number, Ki: number, Kd: number): void {
         let i2cbuf = pins.createBuffer(5)
@@ -271,7 +346,7 @@ namespace BitRacerPro2 {
     * @param Ki 積分增益, eg: 0
     * @param Kd 微分增益, eg: 0
     */
-    //% weight=18 group="PID"
+    //% weight=19 group="PID"
     //% block="PID Pos Kp|%Kp Ki|%Ki Kd|%Kd" advanced=true
     export function setPos(Kp: number, Ki: number, Kd: number): void {
         let i2cbuf = pins.createBuffer(5)
@@ -291,7 +366,7 @@ namespace BitRacerPro2 {
     * @param Ki 積分增益, eg: 0
     * @param Kd 微分增益, eg: 0
     */
-    //% weight=17 group="PID"
+    //% weight=18 group="PID"
     //% block="PID Theta Kp|%Kp Ki|%Ki Kd|%Kd" advanced=true
     export function setTheta(Kp: number, Ki: number, Kd: number): void {
         let i2cbuf = pins.createBuffer(5)
@@ -308,7 +383,7 @@ namespace BitRacerPro2 {
     /**
     * 儲存位置資料
     */
-    //% weight=16 group="PID"
+    //% weight=17 group="PID"
     //% block="savePosData" advanced=true
     export function savePosData(): void {
         pins.i2cWriteNumber(N76_ADDR, 0x4C, NumberFormat.UInt8LE, false)
@@ -316,10 +391,28 @@ namespace BitRacerPro2 {
     /**
     * 儲存角度資料
     */
-    //% weight=15 group="PID"
+    //% weight=16 group="PID"
     //% block="saveThetaData" advanced=true
     export function saveThetaData(): void {
         pins.i2cWriteNumber(N76_ADDR, 0x4D, NumberFormat.UInt8LE, false)
+    }
+    /**
+    * 序列寫入 位置/角度 資料
+    * @param n 資料數量, eg: 1500
+    */
+    //% color=#002050
+    //% weight=15 group="PID"
+    //% block="writePositionOrTheta |%n" advanced=true
+    //% n.min=0 n.max=1500
+    export function writePositionOrTheta(n: number): void {
+        pins.i2cWriteNumber(N76_ADDR, 0x4F, NumberFormat.UInt8LE, false)
+        for (let index = 0; index < n; index++) {
+            let data: number[] = []
+            data[0] = Math.round(pins.i2cReadNumber(16, NumberFormat.Float32LE, false) * 1000000) / 1000000
+            data[1] = Math.round(pins.i2cReadNumber(16, NumberFormat.Float32LE, false) * 1000000) / 1000000
+            serial.writeNumbers(data)
+            basic.pause(10)
+        }
     }
     /**
     * 序列寫入 速度/角速度 資料
@@ -338,31 +431,15 @@ namespace BitRacerPro2 {
             serial.writeNumbers(data)
             basic.pause(10)
         }
-    } 
-    /**
-    * 序列寫入 速度/角速度 資料
-    * @param n 資料數量, eg: 1500
-    */
-    //% color=#002050
-    //% weight=13 group="PID"
-    //% block="writePositionOrTheta |%n" advanced=true
-    //% n.min=0 n.max=1500
-    export function writePositionOrTheta(n:number): void {
-        pins.i2cWriteNumber(N76_ADDR, 0x4F, NumberFormat.UInt8LE, false)
-        for (let index = 0; index < n; index++) {
-            let data: number[] = []
-            data[0] = Math.round(pins.i2cReadNumber(16, NumberFormat.Float32LE, false) * 1000000) / 1000000
-            data[1] = Math.round(pins.i2cReadNumber(16, NumberFormat.Float32LE, false) * 1000000) / 1000000
-            serial.writeNumbers(data)
-            basic.pause(10)
-        }
-    } 
+    }
+
+
     /**
     * 設定輪直徑與輪距
     * @param diameter 輪直徑, eg: 23
     * @param Wheelbase 輪距, eg: 85
     */
-    //% weight=14
+    //% weight=11 group="設定(Setting)"
     //% block="Wheel Diameter(mm)|%diameter Wheelbase(mm)|%Wheelbase" advanced=true
     export function setWheel(diameter: number, Wheelbase: number): void {
         let i2cbuf = pins.createBuffer(5)
@@ -372,13 +449,13 @@ namespace BitRacerPro2 {
         i2cbuf[0] = 0x51
         i2cbuf.setNumber(NumberFormat.Float32LE, 1, Wheelbase)
         pins.i2cWriteBuffer(N76_ADDR, i2cbuf)
-    } 
+    }
     /**
     * 設定加速度與角加速度
     * @param acc 加速度, eg: 5
     * @param alpha 角加速度, eg: 5
     */
-    //% weight=14
+    //% weight=10 group="設定(Setting)"
     //% block="Acceleration(m/s2)|%acc Alpha(m/s2)|%alpha" advanced=true
     export function setAccAlpha(acc: number, alpha: number): void {
         let i2cbuf = pins.createBuffer(5)
@@ -389,97 +466,13 @@ namespace BitRacerPro2 {
         i2cbuf.setNumber(NumberFormat.Float32LE, 1, alpha)
         pins.i2cWriteBuffer(N76_ADDR, i2cbuf)
     }
-    /**
-    * 自走車控制開關
-    * @param status 開關
-    */
-    //% weight=80
-    //% block="carControl |%status" advanced=true
-    export function carControl(status: Switch): void {
-        pins.i2cWriteNumber(N76_ADDR, 0x56 + status, NumberFormat.UInt8LE, false)
-    }
-    /**
-    * 紅外線吸線控制開關
-    * @param status 開關
-    */
-    //% weight=79
-    //% block="IR CorrectionMode |%status" advanced=true
-    export function correctionMode(status: Switch): void {
-        pins.i2cWriteNumber(N76_ADDR, 0x58 + status, NumberFormat.UInt8LE, false)
-    }
-    /**
-    * 位置控制
-    * @param status 控制狀態
-    */
-    //% weight=75
-    //% block="positionControl |%status" advanced=true
-    export function positionFlag(status: control): void {
-        pins.i2cWriteNumber(N76_ADDR, 0x60 + status, NumberFormat.UInt8LE, false)
-    }
-    /**
-    * 轉彎方向
-    * @param status 方向
-    */
-    //% weight=73
-    //% block="turnDirection |%status" advanced=true
-    export function turnDirection(status: direction): void {
-        pins.i2cWriteNumber(N76_ADDR, 0x64 + status, NumberFormat.UInt8LE, false)
-    }
-    /**
-    * 角度控制
-    * @param status 控制狀態
-    */
-    //% weight=74
-    //% block="thetaControl |%status" advanced=true
-    export function thetaFlag(status: control): void {
-        pins.i2cWriteNumber(N76_ADDR, 0x66 + status, NumberFormat.UInt8LE, false)
-    }
-    /**
-    * 讀取角速度命令
-    */
-    //% color=#3dbfa1
-    //% weight=0
-    //% block="readOmegaCommand" advanced=true
-    export function readOmegaCommand(): number {
-        pins.i2cWriteNumber(N76_ADDR, 0x6A, NumberFormat.UInt8LE, false)
-        return pins.i2cReadNumber(N76_ADDR, NumberFormat.Float32LE, false)
-    }
-    /**
-    * 讀取角度命令
-    */
-    //% color=#3dbfa1
-    //% weight=0
-    //% block="readThetaCommand" advanced=true
-    export function readThetaCommand(): number {
-        pins.i2cWriteNumber(N76_ADDR, 0x6B, NumberFormat.UInt8LE, false)
-        return pins.i2cReadNumber(N76_ADDR, NumberFormat.Float32LE, false)
-    }
-    /**
-    * 讀取速度命令
-    */
-    //% color=#3dbfa1
-    //% weight=0
-    //% block="readVelCommand" advanced=true
-    export function readVelCommand(): number {
-        pins.i2cWriteNumber(N76_ADDR, 0x6C, NumberFormat.UInt8LE, false)
-        return pins.i2cReadNumber(N76_ADDR, NumberFormat.Float32LE, false)
-    }
-    /**
-    * 讀取距離命令
-    */
-    //% color=#3dbfa1
-    //% weight=0
-    //% block="readPosCommand" advanced=true
-    export function readPosCommand(): number {
-        pins.i2cWriteNumber(N76_ADDR, 0x6D, NumberFormat.UInt8LE, false)
-        return pins.i2cReadNumber(N76_ADDR, NumberFormat.Float32LE, false)
-    }
+
 
     /**
     * 讀取電池電壓
     */
     //% color=#3dbfa1
-    //% weight=1
+    //% weight=1 group="其他(other)"
     //% block="readBatteryVoltage" advanced=true
     export function readBat(): number {
         pins.i2cWriteNumber(N76_ADDR, 0x2F, NumberFormat.UInt8LE, false)
@@ -489,7 +482,7 @@ namespace BitRacerPro2 {
     * 讀取版本號
     */
     //% color=#3dbfa1
-    //% weight=0
+    //% weight=0 group="其他(other)"
     //% block="readVersion" advanced=true
     export function readVersion(): number {
         pins.i2cWriteNumber(N76_ADDR, 0xFF, NumberFormat.UInt8LE, false)
